91db71f031370fca7985fe0316a60491de812b29
SUSOT <a01076754042@gmail.com>
2025-06-20 09:19:37 +0900

    Del

--- cppTeamPj/Enemy.h
+++ cppTeamPj/Enemy.h
@@ -9,7 +9,6 @@ using std::string;
 class Enemy : public Entity
 {
 private:
-	// Visual을 담아둘 구조체
 	struct Visual
 	{
 		string rightVisual = "←";
@@ -18,11 +17,8 @@ private:
 		string downVisual = "↑";
 	};
 
-	// 움직일 방향 저장해둘 COORD 배열
-	// 차례대로 오른쪽, 왼쪽, 위, 아래
 	COORD directionVectors[4] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
 
-	// enum class에 방향 만들어두기
 	enum class Direction
 	{
 		RIGHT,
@@ -31,11 +27,8 @@ private:
 		DOWN
 	};
 
-	// Visual
 	Visual visual;
-	// 설정전에 기본
 	Direction direction = Direction::RIGHT;
-	//현재 위치 담아둘 COORD
 	COORD currentPos;
 
 public:
@@ -44,13 +37,9 @@ public:
 		SetEnemy();
 	}
 
-	// 에너미 초기값을 셋팅해주는 함수
 	void SetEnemy();
 
-	// 에너미를 생성해주는 함수
 	void Spawn(COORD coord);
 
-	// 에너미를 이동시켜주는 함수
-	// 부모클래스에 있는 Move를 오버라이드해서 사용하는 함수
 	virtual void Move() override;
 };
--- cppTeamPj/EnemyManager.cpp
+++ cppTeamPj/EnemyManager.cpp
@@ -3,15 +3,10 @@
 
 void EnemyManager::EnemyUpdate()
 {
-    // 현재 시간을 받아서 저장하는 변수
-    // std::chrono::high_resolution_clock::now();를 쓰면 현재 클록을 반환한다.
     auto currentTime = std::chrono::high_resolution_clock::now();
     float deltaTime = 0.0f;
-    // 처음 update인지 체크하기
-    // 만약에 안해주면 lastUpdateTime에 이상한값 들어가서 오류날 수 있다
     if (!isFirstUpdate) 
     {
-        // 현재 시간과 전에 측정한 시간의 차이를 deltaTime에 넣어줌
         deltaTime = std::chrono::duration<float>(currentTime - lastUpdateTime).count();
     }
     else 
@@ -29,7 +24,6 @@ void EnemyManager::EnemyUpdate()
     moveTimer += deltaTime;
     if (moveTimer >= moveInterval) 
     {
-        // enemy는 클래스라서 복사비용이 크니깐 참조로 넘기기
         for (Enemy& enemy : enemies) 
         {
             enemy.Move();
@@ -37,7 +31,6 @@ void EnemyManager::EnemyUpdate()
         moveTimer = 0.0f;
     }
 
-    // lastUpdateTime에다가 현재 시간 넣어주기
     lastUpdateTime = currentTime;
 }
 
