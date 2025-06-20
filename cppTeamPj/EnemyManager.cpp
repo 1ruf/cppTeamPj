d0cc41d3fb4969ba676b6f5dc5a9652e65de9da3
SUSOT <a01076754042@gmail.com>
2025-06-20 09:16:22 +0900

    Feat : Enemy

--- cppTeamPj/EnemyManager.cpp
+++ cppTeamPj/EnemyManager.cpp
@@ -43,7 +43,6 @@ void EnemyManager::EnemyUpdate()
 
 void EnemyManager::SpawnEnemy()
 {
-    // emplace_back은 컨테이너 내부에서 직접 객체 생성자를 넘겨준다.
-    // 하지만 push_back은 먼저 생성후에 복사하기 때문에 복사비용이 들어서 emplace_back이 좋다.
-    enemies.emplace_back(Enemy());
+    Enemy enemy;
+    enemies.push_back(enemy);
 }
