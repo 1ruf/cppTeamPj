380bd3021ba3585ccf4f597e61097dc31cf1c13b
SUSOT <a01076754042@gmail.com>
2025-06-22 23:20:00 +0900

    Feat : move spawn logic from Enemy to EnemyManager

--- cppTeamPj/EnemyManager.cpp
+++ cppTeamPj/EnemyManager.cpp
@@ -3,48 +3,47 @@
 
 void EnemyManager::EnemyUpdate()
 {
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    float deltaTime = 0.0f;
-    if (!isFirstUpdate) 
-    {
-        deltaTime = std::chrono::duration<float>(currentTime - lastUpdateTime).count();
-    }
-    else 
-    {
-        isFirstUpdate = false;
-    }
+	auto currentTime = std::chrono::high_resolution_clock::now();
+	float deltaTime = 0.0f;
+	if (!isFirstUpdate)
+	{
+		deltaTime = std::chrono::duration<float>(currentTime - lastUpdateTime).count();
+	}
+	else
+	{
+		isFirstUpdate = false;
+	}
 
-    spawnTimer += deltaTime;
-    if (spawnTimer >= spawnTime) 
-    {
-        SpawnEnemy();
-        spawnTimer = 0.0f;
-    }
+	spawnTimer += deltaTime;
+	if (spawnTimer >= spawnTime)
+	{
+		SpawnEnemy();
+		spawnTimer = 0.0f;
+	}
 
-    moveTimer += deltaTime;
-    if (moveTimer >= moveTime) 
-    {
-        for (Enemy& enemy : enemies) 
-        {
-            if (!enemy.CheckShield())
-            {
-                enemy.Move();   
-            }
-            else
-            {
-                enemies.erase(enemies.begin());
-            }
-        }
-        moveTimer = 0.0f;
-    }
+	moveTimer += deltaTime;
+	if (moveTimer >= moveTime)
+	{
+		for (Enemy& enemy : enemies)
+		{
+			if (!enemy.CheckShield())
+			{
+				enemy.Move();
+			}
+			else
+			{
+				//enemies.erase(enemies.begin());
+			}
+		}
+		moveTimer = 0.0f;
+	}
 
-    lastUpdateTime = currentTime;
+	lastUpdateTime = currentTime;
 }
 
 void EnemyManager::SpawnEnemy()
 {
-    for (int i = 0; i < 3; ++i)
-    {
-        enemies.emplace_back();
-    }
+	enemies.emplace_back();
+	enemies.back().SetEnemy();
+	enemies.back().Render();
 }
