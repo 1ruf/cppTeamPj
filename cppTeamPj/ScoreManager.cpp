6a2606f090c6a7b9d980275d97251463d936c74b
SUSOT <a01076754042@gmail.com>
2025-06-27 10:09:38 +0900

    Feat : Change Score UI

--- cppTeamPj/ScoreManager.cpp
+++ cppTeamPj/ScoreManager.cpp
@@ -1,5 +1,8 @@
-#include "ScoreManager.h"
+ï»¿#include "ScoreManager.h"
 #include "Console.h"
+#include <fcntl.h>
+#include <io.h>
+#include <sstream>
 
 void ScoreManager::ScoreUp(int plusScore)
 {
@@ -10,8 +13,36 @@ void ScoreManager::ScoreUp(int plusScore)
 void ScoreManager::ScoreRender()
 {
 	COORD coord = GetConsoleResolution();
-	coord.X *= 0.7;
-	coord.Y *= 0.2;
-	Gotoxy(coord.X, coord.Y);
-	cout << "ÇöÀç Á¡¼ö : " << score;
+	Gotoxy(0, 0);
+	int prevmode = _setmode(_fileno(stdout), _O_U16TEXT);
+	wcout << L" _____ _____ _____ _____ _____" << endl;
+	wcout << L"|   __|     |     | __  |   __|" << endl;
+	wcout << L"|__   |   --|  |  |    -|   __|" << endl;
+	wcout << L"|_____|_____|_____|__|__|_____|" << endl;
+	wcout << L" _____________________________ " << endl;
+	wcout << L"|                             |" << endl;
+	wcout << L"|                             |" << endl;
+	wcout << L"|                             |" << endl;
+	wcout << L"|                             |" << endl;
+	wcout << L"|_____________________________|" << endl;
+
+	coord.X = 2;
+	coord.Y = 5;
+	scoreText = std::to_string(score);
+    for (char c : scoreText)
+    {
+        int idx = c - '0';
+
+        std::wistringstream stream(scoreTexts[idx]);
+        std::wstring line;
+        int lineNum = 0;
+
+        while (std::getline(stream, line)) {
+            Gotoxy(coord.X, coord.Y + lineNum);
+            wcout << line;
+            lineNum++;
+        }
+        coord.X += 6;
+    }
+	int curmode = _setmode(_fileno(stdout), prevmode);
 }
--- cppTeamPj/ScoreManager.h
+++ cppTeamPj/ScoreManager.h
@@ -1,8 +1,56 @@
 #pragma once
+
+#include <string>
+
 class ScoreManager
 {
 private:
 	int score = 0;
+	std::string scoreText;
+
+	// Raw String Litaral
+	std::wstring scoreTexts[10] = {
+		LR"( ___ 
+|   | 
+| | | 
+|___| )",
+		LR"( ___   
+|_  |  
+ _| | 
+|____|)",
+		LR"( ___ 
+|_  | 
+|  _| 
+|___| )",
+		LR"( ___ 
+|_  | 
+|_  | 
+|___| )",
+		LR"( ___ 
+| | | 
+|_  | 
+  |_| )",
+		LR"( ___ 
+|  _| 
+|_  | 
+|___| )",
+		LR"( ___ 
+|  _| 
+| . | 
+|___| )",
+		LR"( ___ 
+|_  | 
+  | | 
+  |_| )",
+		LR"( ___ 
+| . | 
+| . | 
+|___| )",
+		LR"( ___ 
+| . | 
+|_  | 
+|___| )"
+	};
 
 public:
 	void ScoreUp(int plusScore);
