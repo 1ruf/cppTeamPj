f6cb09a239a9cdd4d5d6ef6349836c2d1f2f89b0
고한결 <gyeol0411@naver.com>
2025-06-28 21:13:00 +0900

    .update Credit

--- cppTeamPj/CreditScene.cpp
+++ cppTeamPj/CreditScene.cpp
@@ -21,38 +21,11 @@ void CreditSceneRender()
 
 	Gotoxy(3, 1);
 	cout << "< 종료:ESC >" << endl;
-	Gotoxy(0, consoleSize.Y / 2 - 20);
+	Gotoxy(0, consoleSize.Y / 2 - 15);
 	cout << "기획자 : 고한결, 이찬민" << endl;
 	cout << "제작자 : 고한결, 이찬민" << endl;
 	cout << "디자인 : 고한결, 이찬민\n\n\n" << endl;
 	cout << "???:고한결이랑 이찬민이 다만들었네 ㄷㄷ 나머지 팀원들 다 뭐함?" << endl;
-
-	cout << "                          /-                                   " << endl;
-	cout << "                          \|                                   " << endl;
-	cout << "                        __________                             " << endl;
-	cout << "                                  ---___                       " << endl;
-	cout << "                                        ---\                   " << endl;
-	cout << "                                            \/--/              " << endl;
-	cout << "                                            /  /--\            " << endl;
-	cout << "                                           /  /    \           " << endl;
-	cout << "                                          /  /      |          " << endl;
-	cout << "                                         /  /       |          " << endl;
-	cout << "                                         | ||       |          " << endl;
-	cout << "                                         \ \|       \          " << endl;
-	cout << "                                          \ |        |         " << endl;
-	cout << "                                           -|        \         " << endl;
-	cout << "                                           /         |         " << endl;
-	cout << "                                          |          |         " << endl;
-	cout << "                                         /           \         " << endl;
-	cout << "                                         |            \        " << endl;
-	cout << "                                        /              \       " << endl;
-	cout << "                                        |              |       " << endl;
-	cout << "                                        |              |       " << endl;
-	cout << "                                         \             /       " << endl;
-	cout << "                                          \-|         /        " << endl;
-	cout << "                                            /      /--         " << endl;
-	cout << "                                           ---------           " << endl;
-	cout << "                                                   " << endl;
 	Gotoxy(consoleSize.X / 2, consoleSize.Y / 2 + 20);
 	cout << "\n플레이 해주셔서 감사합니다!" << endl;
 }
